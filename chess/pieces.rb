90f96fec90ffd46685872aa0172b3683459b53bb
smuushi <mishih780@gmail.com>
2023-01-31 15:31:04 -0500

    woop

--- chess/pieces.rb
+++ chess/pieces.rb
@@ -1,9 +1,18 @@
 class Piece
 
+  attr_reader :color, :board_inst
+  attr_accessor :pos
+
   def initialize(color, board_inst, pos) # color: symbol, board_inst, pos: 2-ele-arr
     @color = color
     @board = board_inst
     @pos = pos
+  end
+
+  def to_s 
+
+    puts "color: #{color}, pos: #{pos}"
+
   end
 
   
