bfba56c74e4fe577c6ba45cf74460751052e7b16
smuushi <mishih780@gmail.com>
2023-01-31 16:30:12 -0500

    make board ish

--- chess/board.rb
+++ chess/board.rb
@@ -3,11 +3,17 @@ require_relative 'pieces'
 class Board
 
   def self.make_board
-    return Array.new(8) {Array.new(8) {Piece.new}}
+    grid = Array.new(8) {Array.new(8) {Piece.new}}
+    
+    grid[2..5].each do |row|
+      row.each_index { |i| row[i] = nil }
+    end
+
+    return grid
   end
 
   def initialize
-    @rows = Board.make_board  # 2-d array with nils
+    @rows = Board.make_board  # 2-d array with nils in rows 2..5
   end
 
   def [](pos)
@@ -21,8 +27,8 @@ class Board
   end
 
   def move_piece(start_pos, end_pos) # NILner was here
-    raise ArgumentError.new('No piece in start pos')
-    raise ArgumentError.new('Cannot move to end pos')
+    raise ArgumentError.new('No piece in start pos') if self[start_pos] == nil
+    raise ArgumentError.new('Cannot move to end pos') if self[end_pos] != nil
     self[start_pos], self[end_pos] = nil, self[start_pos]
   end
 
