f5395863c19ea007cf95ca8d5dd791545f689f10
smuushi <mishih780@gmail.com>
2023-02-01 17:16:13 -0500

    asdf

--- chess/board.rb
+++ chess/board.rb
@@ -6,20 +6,21 @@ require_relative 'pieces/move_mods'
 require_relative 'pieces/queen'
 require_relative 'pieces/rook'
 require_relative 'pieces/bishop'
+require_relative "pieces/pawn"
 
 
 class Board
 
   def make_board
-    grid = Array.new(8) {Array.new(8)}
+    grid = Array.new(8) {Array.new(8) {NullPiece.instance}}
     
     # grid[0..1].each do |row|
     #   row.each_index { |i| row[i] = Piece.new(:white, self, [0,0])}
     # end
 
-    grid[2..5].each do |row|
-      row.each_index { |i| row[i] = NullPiece.instance }
-    end
+    # grid[2..5].each do |row|
+    #   row.each_index { |i| row[i] = NullPiece.instance }
+    # end
 
     # grid[6..7].each do |row|
     #   row.each_index { |i| row[i] = Piece.new(:black, self, [0,0])}
@@ -29,22 +30,26 @@ class Board
   end
 
   def initialize
-    @rows = self.make_board  # 2-d array with nils in rows 2..5
-
+    @grid = self.make_board  # 2-d array with nils in rows 2..5
+    self.make_pawns_1
+    self.make_pawns_2
   end
 
-  def make_pawns
-    
+  def make_pawns_1
+    @grid[1].each_index { |col| @grid[1, col] = Pawn.new(:white, self, [1, col])}
+  end
+  def make_pawns_2
+    @grid[6].each_index { |col| @grid[6, col] = Pawn.new(:black, self, [6, col])}
   end
 
   def [](pos)
     row, col = pos[0], pos[1]
-    return @rows[row][col]
+    return @grid[row][col]
   end
 
   def []=(pos, val)
     row, col = pos[0], pos[1]
-    @rows[row][col] = val
+    @grid[row][col] = val
   end
 
   def move_piece(start_pos, end_pos) # NILner was here
--- chess/pieces/pawn.rb
+++ chess/pieces/pawn.rb
@@ -2,7 +2,7 @@ require_relative 'piece'
 
 class Pawn < Piece
 
-  
+  private
   
   def forward_dir
     if self.color == :white
@@ -34,6 +34,39 @@ class Pawn < Piece
     else
       ostensible_steps << [row + forward_dir, col]
     end
+
+    return ostensible_steps
+  end
+
+  def side_attacks
+
+    ostensible_steps = []
+    row, col = self.pos
+
+    if self.board[row + forward_dir, col + 1] != NullPiece.instance 
+      if capturable?(self.board[row + forward_dir, col + 1])
+        ostensible_steps << [row + forward_dir, col + 1]
+      end
+    end
+    
+    if self.board[row + forward_dir, col - 1]
+      if capturable?(self.board[row + forward_dir, col - 1])
+        ostensible_steps << [row + forward_dir, col - 1]
+      end
+    end   
+
+    return ostensible_steps
+  end
+
+  def capturable?(piece_instance)
+    return true if piece_instance.color != self.color && piece_instance.color != nil
+    false
+  end
+
+public
+
+  def moves
+    forward_steps + side_attacks
   end
 
   def symbol
