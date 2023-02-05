13c2f444816d13a65c05f76f0b4a0da30b4f3104
smuushi <mishih780@gmail.com>
2023-02-04 21:28:24 -0500

    board set up

--- chess/board.rb
+++ chess/board.rb
@@ -13,40 +13,80 @@ class Board
 
   def make_board
     grid = Array.new(8) {Array.new(8) {NullPiece.instance}}
-    
-    # grid[0..1].each do |row|
-    #   row.each_index { |i| row[i] = Piece.new(:white, self, [0,0])}
-    # end
-
-    # grid[2..5].each do |row|
-    #   row.each_index { |i| row[i] = NullPiece.instance }
-    # end
-
-    # grid[6..7].each do |row|
-    #   row.each_index { |i| row[i] = Piece.new(:black, self, [0,0])}
-    # end
-
+   
     return grid
   end
 
   def initialize
     @grid = self.make_board  # 2-d array with nils in rows 2..5
-    self.make_pawns_1
-    self.make_pawns_2
+    self.make_white_side
+    self.make_black_side
   end
 
-  def make_pawns_1
+  def make_white_side
+    @grid[0].each_index do |col| 
+      
+      new_rook = Rook.new(:white, self, [0, col]) if col == 0 || col == 7
+      self.[]=([0,col],new_rook) if col == 0 || col == 7
+
+      new_knight = Knight.new(:white, self, [0,col]) if col == 1 || col == 6
+      self.[]=([0,col],new_knight) if col == 1 || col == 6
+
+      new_bishop = Bishop.new(:white, self, [0, col]) if col == 2 || col == 5
+      self.[]=([0,col],new_bishop) if col == 2 || col == 5
+
+      new_queen = Queen.new(:white, self, [0, col]) if col == 3
+      self.[]=([0,col],new_queen) if col == 3
+
+      new_king = King.new(:white, self, [0,col]) if col == 4
+      self.[]=([0,col],new_king) if col == 4
+
+    end
+
     @grid[1].each_index do |col| 
       new_pawn = Pawn.new(:white, self, [1, col])
       self.[]=([1,col],new_pawn)
     end
+
   end
-  def make_pawns_2
+
+  def make_black_side
+
     @grid[6].each_index do |col|
       new_pawn = Pawn.new(:black, self, [6,col]) 
       self.[]=([6,col], new_pawn)
     end
+
+    @grid[7].each_index do |col|
+      if col == 0 || col == 7
+        new_rook = Rook.new(:black, self, [7, col])
+        self.[]=([7,col],new_rook)
+      end
+
+      if col == 1 || col == 6
+        new_knight = Knight.new(:black, self, [7,col]) 
+        self.[]=([7,col],new_knight) 
+      end
+
+      if col == 2 || col == 5
+        new_bishop = Bishop.new(:black, self, [7, col]) 
+        self.[]=([7,col],new_bishop)
+      end
+
+      if col == 3
+        new_queen = Queen.new(:black, self, [7, col])
+        self.[]=([7,col],new_queen)
+      end
+
+      if col == 4
+        new_king = King.new(:black, self, [7,col])
+        self.[]=([7,col],new_king)
+      end
+
+    end
+
   end
+
 
   def [](row,col)
     return @grid[row][col]
@@ -58,9 +98,9 @@ class Board
   end
 
   def move_piece(start_pos, end_pos) # NILner was here
-    raise ArgumentError.new('No piece in start pos') if self[start_pos] == nil
-    raise ArgumentError.new('Cannot move to end pos') if self[end_pos] != nil
-    self[start_pos], self[end_pos] = nil, self[start_pos]
+    raise ArgumentError.new('No piece in start pos') if self[start_pos] == NullPiece.instance
+    raise ArgumentError.new('Cannot move to end pos') if self[end_pos] != NullPiece.instance
+    self[start_pos], self[end_pos] = NullPiece.instance, self[start_pos]
   end
 
 
